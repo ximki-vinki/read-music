b80d80cd841dfe2334ee510e069ffa141752d5a4
ILJA MANYUTIN <i.manytuin@auchan.ru>
2025-11-19 14:45:08 +0300

    Попробовать чтение

--- ReadMusic.App/Program.fs
+++ ReadMusic.App/Program.fs
@@ -1,4 +1,14 @@
-﻿[<EntryPoint>]
-let main argv =
-    printfn "Hello, Ilja! "
-    0  
+﻿open ReadMusic.App
+
+[<EntryPoint>]
+let main _ =
+    let path = "/media/ximki-vinki/C487EA472D10D620/music/Era/1996 - Era/01 - Era.flac"
+    match File.tryReadBytes path with
+    | Ok bytes -> 
+        match File.detectFormat bytes with
+        | File.Mp3   -> printfn "MP3"
+        | File.Flac  -> printfn "FLAC"
+        | File.Unknown -> printfn "???"
+    | Error (File.FileNotFound p) -> printfn "Not found: %s" p
+    | Error (File.IOError m)      -> printfn "IO: %s" m
+    0
