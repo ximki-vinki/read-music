9e3c23ec1c0bb84d4dccb5ff2a9b11e9bc2675c6
ILJA MANYUTIN <i.manytuin@auchan.ru>
2025-11-23 12:38:22 +0300

    Вывести чтение в отдельную функцию

--- ReadMusic.App/Program.fs
+++ ReadMusic.App/Program.fs
@@ -1,14 +1,18 @@
-﻿open ReadMusic.App
+﻿module Program
+
+open System.IO
+open ReadMusic.App.File.Header
+open ReadMusic.App.Domain.Meta
 
 [<EntryPoint>]
 let main _ =
     let path = "/media/ximki-vinki/C487EA472D10D620/music/Era/1996 - Era/01 - Era.flac"
-    match File.tryReadBytes path with
-    | Ok bytes -> 
-        match File.detectFormat bytes with
-        | File.Mp3   -> printfn "MP3"
-        | File.Flac  -> printfn "FLAC"
-        | File.Unknown -> printfn "???"
-    | Error (File.FileNotFound p) -> printfn "Not found: %s" p
-    | Error (File.IOError m)      -> printfn "IO: %s" m
-    0
+
+    match readHeader path with
+    | Error msg ->
+        eprintfn $"Error %s{msg}"
+        1
+    | Ok header ->
+        let format = detect header
+        printfn $"%s{Path.GetFileName path}: %A{format}"
+        0
