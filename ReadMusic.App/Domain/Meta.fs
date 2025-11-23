6e4c9fb9dde8d1f785a323a5bf675dc8a853820b
ILJA MANYUTIN <i.manytuin@auchan.ru>
2025-11-23 14:20:59 +0300

    Пернести чтение на teg

--- ReadMusic.App/Domain/Meta.fs
+++ ReadMusic.App/Domain/Meta.fs
@@ -1,23 +1,14 @@
 module ReadMusic.App.Domain.Meta
 
-let requiredHeaderSize = 16
+open ReadMusic.App.Domain
+open TagLib
 
-module private Bytes =
-    let startsWith (prefix: byte[]) (data: byte[]) : bool =
-        prefix.Length <= data.Length &&
-        Array.forall2 (=) prefix (data.[.. prefix.Length - 1])
-
-    let B (s: string) : byte[] = System.Text.Encoding.ASCII.GetBytes s
-
-let getSignature = function
-    | Mp3   -> Bytes.B "ID3"
-    | Flac  -> Bytes.B "fLaC"
-    | Ogg   -> Bytes.B "OggS"
-    | Unknown -> [||]
-
-let private supportedFormats = [ Mp3; Flac; Ogg ]
-
-let detect (bytes: byte[]) : AudioFormat =
-    supportedFormats
-    |> List.tryFind (fun fmt -> Bytes.startsWith (getSignature fmt) bytes)
-    |> Option.defaultValue Unknown
+let detect (path: string) : AudioFormat =
+    try
+        use file = File.Create path
+        match file with
+        | :? Flac.File -> Flac
+        | :? Mpeg.File -> Mp3
+        | :? Ogg.File  -> Ogg
+        | _ -> Unknown
+    with _ -> Unknown
--- ReadMusic.App/Program.fs
+++ ReadMusic.App/Program.fs
@@ -1,18 +1,12 @@
 ﻿module Program
 
 open System.IO
-open ReadMusic.App.File.Header
 open ReadMusic.App.Domain.Meta
 
 [<EntryPoint>]
 let main _ =
     let path = "/media/ximki-vinki/C487EA472D10D620/music/Era/1996 - Era/01 - Era.flac"
 
-    match readHeader path with
-    | Error msg ->
-        eprintfn $"Error %s{msg}"
-        1
-    | Ok header ->
-        let format = detect header
-        printfn $"%s{Path.GetFileName path}: %A{format}"
-        0
+    let format = detect path
+    printfn $"%s{Path.GetFileName path}: %A{format}"
+    0
